a8c61cb9b54f619604fe0682f51f1e4ec85e59b5
Prayoch Rujira <jay@odd-e.co.th>
2016-03-23 11:55:51 +0700

    solve changing text

--- app/src/main/java/com/example/jacksw/mygrade/MainActivity.java
+++ app/src/main/java/com/example/jacksw/mygrade/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.jacksw.mygrade;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onSubmit(View view) {
+        TextView scoreText = (TextView)findViewById(R.id.gradeView);
+
+        String grade = "B";
+        scoreText.setText(grade);
     }
 
 }
