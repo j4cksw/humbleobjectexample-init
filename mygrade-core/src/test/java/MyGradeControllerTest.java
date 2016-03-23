81081d9d7061d8fc0a5cf97a1ee4a0e6ee37c001
Prayoch Rujira <jay@odd-e.co.th>
2016-03-23 18:56:32 +0700

    add the first test of MyGradeController

---
+++ mygrade-core/src/test/java/MyGradeControllerTest.java
@@ -0,0 +1,13 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by JackSW on 3/23/2016 AD.
+ */
+public class MyGradeControllerTest {
+
+    @Test
+    public void onSubmit_should_call_getScore_from_activity(){
+        Assert.fail("Not yet implemented");
+    }
+}
