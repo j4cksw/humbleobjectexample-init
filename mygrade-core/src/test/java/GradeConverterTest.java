99b0d29e0fd6c60f6cdd10a4e2662031e5a071bf
Prayoch Rujira <jay@odd-e.co.th>
2016-03-25 14:41:07 +0700

    extract instantiating GradeConverter to be filed variable and create its instance in setup function

--- mygrade-core/src/test/java/GradeConverterTest.java
+++ mygrade-core/src/test/java/GradeConverterTest.java
@@ -1,28 +1,31 @@
 import com.example.GradeConverter;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class GradeConverterTest {
 
+    private GradeConverter converter;
+
+    @Before
+    public void setUp() throws Exception {
+        converter = new GradeConverter();
+    }
+
     @Test
     public void fromScore_given_90_shouldReturn_A() {
-        GradeConverter converter = new GradeConverter();
-
         Assert.assertEquals(converter.fromScore(90), "A");
     }
 
     @Test
     public void fromScore_given_0_shouldReturn_F() {
-        GradeConverter converter = new GradeConverter();
 
         Assert.assertEquals(converter.fromScore(0), "F");
     }
 
     @Test
     public void fromScore_given_50_shouldReturn_D() {
-        GradeConverter converter = new GradeConverter();
-
         Assert.assertEquals(converter.fromScore(50), "D");
     }
 }
